a27cb271d9aa32d9027f02d7312293f7c1c96040
Ricardo Ribeiro <102837467+MatosRicardo@users.noreply.github.com>
2024-11-28 10:21:47 -0300

    Em andamento

--- SistemaEscolar/SistemaEscolar/src/main/java/com/example/SistemaEscolar/model/Presenca.java
+++ SistemaEscolar/SistemaEscolar/src/main/java/com/example/SistemaEscolar/model/Presenca.java
@@ -2,6 +2,8 @@ package com.example.SistemaEscolar.model;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "tb_presenca")
 public class Presenca {
@@ -9,4 +11,24 @@ public class Presenca {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "data_presenca")
+    private Date dataPresenca;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDataPresenca() {
+        return dataPresenca;
+    }
+
+    public void setDataPresenca(Date dataPresenca) {
+        this.dataPresenca = dataPresenca;
+    }
 }
