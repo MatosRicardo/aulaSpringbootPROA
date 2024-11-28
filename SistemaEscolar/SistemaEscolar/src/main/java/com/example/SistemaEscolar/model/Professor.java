8fca0bcf2fb8c44983a45804abcee1246e7b8e08
Ricardo Ribeiro <102837467+MatosRicardo@users.noreply.github.com>
2024-11-28 10:34:08 -0300

    Em andamento

--- SistemaEscolar/SistemaEscolar/src/main/java/com/example/SistemaEscolar/model/Professor.java
+++ SistemaEscolar/SistemaEscolar/src/main/java/com/example/SistemaEscolar/model/Professor.java
@@ -1,4 +1,46 @@
 package com.example.SistemaEscolar.model;
 
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_professor")
+
 public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome_professor")
+    private String nomeProfessor;
+
+    @Column(name = "email_professor")
+    private String emailProfessor;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public void setNomeProfessor(String nomeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+    }
+
+    public String getEmailProfessor() {
+        return emailProfessor;
+    }
+
+    public void setEmailProfessor(String emailProfessor) {
+        this.emailProfessor = emailProfessor;
+    }
 }
--- SistemaEscolar/SistemaEscolar/src/main/java/com/example/SistemaEscolar/model/Sala.java
+++ SistemaEscolar/SistemaEscolar/src/main/java/com/example/SistemaEscolar/model/Sala.java
@@ -1,4 +1,42 @@
 package com.example.SistemaEscolar.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "tb_sala")
 public class Sala {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome_sala")
+    private String nomeSala;
+
+    @Column(name = "nome_sala")
+    private int capacidadeSala;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNomeSala() {
+        return nomeSala;
+    }
+
+    public void setNomeSala(String nomeSala) {
+        this.nomeSala = nomeSala;
+    }
+
+    public int getCapacidadeSala() {
+        return capacidadeSala;
+    }
+
+    public void setCapacidadeSala(int capacidadeSala) {
+        this.capacidadeSala = capacidadeSala;
+    }
 }
